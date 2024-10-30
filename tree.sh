4d449938ab0b2700227371579da7b75b93c15be7
minijibli <kanthamjib@gmail.com>
2024-10-30 15:36:54 +0700

    30/10/2567

---
+++ tree.sh
@@ -0,0 +1,23 @@
+cloud-image-classifier/
+│
+├── data/
+│   └── dataset_loader.py		# dataset input and handling
+│
+├── models/
+│   ├── cnn_model.py			# CNN training
+│   └── model_evaluation.py		# model evaluation
+│
+├── api/
+│   └── inference_api.py		# build API for model calling process
+│
+├── web/
+│   └── app.py					# Frontend by Streamlit or Flask
+│
+├── utils/
+│   └── helper_functions.py		# add functions
+│
+├── main.py						# main:>
+│
+├── requirements.txt			# libraries lists
+│
+└── README.md
